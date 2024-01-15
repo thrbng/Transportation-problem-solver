c622597ddb4bf0d77115b2ce0589dffc0e5f3ae8
BENGUELLA Mohammed Elamine Tahar <benguella.tahar@gmail.com>
2024-01-15 19:58:47 +0100

    fix add values

--- app/src/main/java/com/example/transportationproblemsolver/AddValuesActivity.kt
+++ app/src/main/java/com/example/transportationproblemsolver/AddValuesActivity.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
 class AddValuesActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_values)
